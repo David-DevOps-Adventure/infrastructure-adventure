19d02d18aace03e518ad5bd46e3524891aa8b33e
David Del Sol <daviddelsol1998@gmail.com>
2023-04-26 17:08:39 -0500

    feat: Setup Terraform workflow
    - setup terraform configuration files
    - created github actions workflow

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+# locals
+locals {
+  resource_group_name = "rg-${var.naming_prefix}"
+}
+
+# resources
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = var.location
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+# vars
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "adevnture-prod-01"
+}
