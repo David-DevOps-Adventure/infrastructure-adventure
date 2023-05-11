372891e4b1424dfc0c0010139849a41feba71460
David Del Sol <daviddelsol1998@gmail.com>
2023-05-11 10:26:10 -0500

    feat: Separate test and prod environments
    - created test and prod RGs
    - created test and prod static apps

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,35 @@
 # locals
 locals {
-  resource_group   = "rg-${var.naming_prefix}-${var.location}-01"
-  static_front_end = "staticfe-${var.naming_prefix}-${var.location}-01"
+  test_resource_group   = "rg-${var.naming_prefix}-test-${var.location}-01"
+  test_static_front_end = "stapp-frontend-${var.naming_prefix}-test-${var.location}-01"
+  prod_resource_group   = "rg-${var.naming_prefix}-prod-${var.location}-01"
+  prod_static_front_end = "stapp-frontend-${var.naming_prefix}-prod-${var.location}-01"
 }
 
 # resources
-resource "azurerm_resource_group" "rg" {
-  name     = local.resource_group
+resource "azurerm_resource_group" "test_rg" {
+  name     = local.test_resource_group
   location = var.location
-  tags     = var.tags
+  tags     = var.test_tags
 }
 
-resource "azurerm_static_site" "static_frontend" {
-  name                = local.static_front_end
-  resource_group_name = local.resource_group
+resource "azurerm_static_site" "test_static_front_end" {
+  name                = local.test_static_front_end
+  resource_group_name = local.test_resource_group
+  location            = var.location
+  sku_tier            = "Free"
+  sku_size            = "Free"
+}
+
+resource "azurerm_resource_group" "prod_rg" {
+  name     = local.prod_resource_group
+  location = var.location
+  tags     = var.prod_tags
+}
+
+resource "azurerm_static_site" "prod_static_front_end" {
+  name                = local.prod_static_front_end
+  resource_group_name = local.prod_resource_group
   location            = var.location
   sku_tier            = "Free"
   sku_size            = "Free"
